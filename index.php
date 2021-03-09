8b0819302a2375209a751a857fc65c8a9a790a51
Hien Pham <daisypham@Hiens-MacBook-Pro-3.local>
2021-03-09 12:18:12 +0100

    [ADD] - add to change navigation

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    if(isset($_GET['action'])){
+        $page = $_GET['action'];
+    }elseif(count($_GET) == 0){
+        $page = 'index';
+    }
+    switch ($page) {
+        case 'index':
+            include('./vue/accueil_intmarket.html');
+            break;
+        case 'formulaire_connexion':
+            include('./vue/formulaire_connexion.html');
+            break;
+        case 'formulaire_inscription':
+            include('./vue/formulaire_inscription.html');
+            break;
+        case 'backend':
+            include('./vue/formulaire_afficher_backend.html');
+            break;
+        default:
+            include('./controller/erreur.php');
+            break;
+    }
+?>
